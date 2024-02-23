d4762a9b683e176e8655198d583c7059cffe8c26
Ricardo Magalhães <ricardomagalhaes.dev@gmail.com>
2024-02-23 16:10:03 +0000

    Foram adicionadas duas variaveis e imprimidas de diferentes maneiras

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -6,6 +6,15 @@ namespace ManyStrings
     {
         static void Main(string[] args)
         {
+
+        ///exercicio 6
+        double xx =1.23456;
+        int ii = 19;
+        Console.WriteLine($"{xx:f2}" + " " + $"{xx,p1}" + " " + $"{ii,X}"
+         + " " + $"{ii,C}"); 
+        //formatação de string com variáveis e valores
+
+
         ///Variaveis tipo string
         string s = "Bom dia Armindo, \t  olha uma estrela \u2605";
         string a = "\u2742";
