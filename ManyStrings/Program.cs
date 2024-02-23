46a418bf559f63a5db0a7d08c6f387690b040c1d
Ricardo Magalhães <ricardomagalhaes.dev@gmail.com>
2024-02-23 14:14:51 +0000

    Foram criadas mais variaveis do tipo string

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -10,7 +10,11 @@ namespace ManyStrings
         string s = "Bom dia Armindo, \t  olha uma estrela \u2605";
         string a = "\u2742";
         string z = "Bom dia Jorge";
-        Console.WriteLine( s + " " + a + " " + z )
+        Console.WriteLine( s + " " + a + " " + z );
+        ///Mais variaveis do tipo string
+        string conc = "Um" + "Dois" + "tres";
+        string interp = $"valor de x e' {x}";
+        Console.WriteLine(conc + " " + interp);
         }
     }
 }
