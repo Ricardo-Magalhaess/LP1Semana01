88b4917cb2689d19d64d0d9b6d908cda1d65c90e
Ricardo Magalhães <ricardomagalhaes.dev@gmail.com>
2024-02-23 14:08:18 +0000

    foram criadas variaveis do tipo string

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        ///Variaveis tipo string
+        string s = "Bom dia Armindo, \t  olha uma estrela \u2605";
+        string a = "\u2742";
+        string z = "Bom dia Jorge";
+        Console.WriteLine( s + " " + a + " " + z )
+        }
+    }
+}
