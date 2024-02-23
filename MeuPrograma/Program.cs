d069fc66f46b0910642188273108227f76d5200b
Ricardo Magalhães <ricardomagalhaes.dev@gmail.com>
2024-02-23 12:55:44 +0000

    foram adicionadas tags///

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,16 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// O programa executa 2 instruções de texto.
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// a primeira função diz Hello world
+        /// e a segunda diz Bye world.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
