1f1e2eda3ff8dc4209c7d9db55435d2bbb5e871d
Ricardo Magalhães <ricardomagalhaes.dev@gmail.com>
2024-02-23 13:40:52 +0000

    foram criadas variaveis char com diferentes valores Unicode

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -11,6 +11,10 @@ namespace VariousTypes
            short short_int = 10;
            sbyte sigma = 6;
            Console.WriteLine( num_inteiro + "" + short_int + "" + sigma);
+           ///Variaveis char
+           char estrela = '\u2605'
+           char simbolo = '\u2742'
+           Console.WriteLine("" + estrela + " " + simbolo);
         }
     }
 }
