e2a5a2cd54181db9092a49b85d4da3a2a285d545
Ricardo Magalhães <ricardomagalhaes.dev@gmail.com>
2024-02-23 13:31:42 +0000

    foram criadas variaveis inteiras de diferentes tipos com literais adqueados

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           ///Variaveis inteiras
+           int num_inteiro = 8;
+           short short_int = 10;
+           sbyte sigma = 6;
+           Console.WriteLine( num_inteiro + "" + short_int + "" + sigma);
+        }
+    }
+}
